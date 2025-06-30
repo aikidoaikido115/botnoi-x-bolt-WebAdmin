bff0c7da9325bdf506807b2d739ddc97f0a9c75d
SaRai <pasinlao2546@gmail.com>
2025-06-30 16:09:16 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:3000"],
+    allow_origins=["http://localhost:5173","http://localhost:3000","https://mybooking-front.ngrok.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
